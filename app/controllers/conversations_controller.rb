3cc3600df29a138fe905ce8eacbd5c4cd83b9fa2
Paulina Kisielewska <paulina.kisielewska6@gmail.com>
2024-03-05 17:13:09 +0100

    API call working ver 1

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,11 +1,9 @@
 class ConversationsController < ApplicationController
   def show
-    client = OpenAI::Client.new
-    chaptgpt_response = client.chat(parameters: {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Lead me through a simple conversation that takes place at/or is about #{@topic.name}.
-        here is a description #{@topic.description}. The conversation is for someone who is #{topic.level} in German."}]
-    })
-    @content = chaptgpt_response["choices"][0]["message"]["content"]
+    @conversation = Conversation.find(params[:id])
+    @bot = BotMessage.new
+    @bot.conversation = @conversation
+    @bot.save
+    @bot.generate_content(@conversation.topic)
   end
 end
--- app/models/bot_message.rb
+++ app/models/bot_message.rb
@@ -1,3 +1,17 @@
 class BotMessage < ApplicationRecord
   belongs_to :conversation
+
+  def generate_content(topic)
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: "Have a conversation with me that takes place at/or is about #{topic.name}.
+        here is a description #{topic.description}. I am #{topic.level} in German. Please only send the welcome message
+        and wait for me to reply."}]
+    })
+    new_content = chaptgpt_response["choices"][0]["message"]["content"]
+
+    update(content: new_content)
+    return new_content
+  end
 end
