889034b4979742f470c05187e841242065a1d8ec
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-03-06 10:23:31 +0100

    styling radio buttons topics index pasge

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,4 +6,16 @@ class ConversationsController < ApplicationController
     @bot.save
     @bot.generate_content(@conversation.topic)
   end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    @conversation.save
+    redirect_to conversation_path(@conversation)
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:topic_id, :user_id)
+  end
 end
