4aaccff446e52a3e070a28a04dc8a4cb70633ff9
Paulina Kisielewska <paulina.kisielewska6@gmail.com>
2024-03-07 12:26:06 +0100

    System message with context added

--- app/models/user_message.rb
+++ app/models/user_message.rb
@@ -5,8 +5,10 @@ class UserMessage < ApplicationRecord
   belongs_to :bot_message
 
   def generate_bot
-    messages = []
-
+    messages = [{ role: "system", content: "You are leading a conversation with the user in German that takes place at/or is about #{conversation.topic.name}.
+      here is a description #{conversation.topic.description}. You are not a bot, but a real person. I am #{conversation.topic.level}. Please only send the welcome message
+      and wait for me to reply. Please send short messages of maximum 3 sentences. The conversation should end after 10 replies" }]
+    
     # Fetch all bot messages and user messages associated with the conversation
     bot_messages = BotMessage.where(conversation_id: conversation.id).order(created_at: :asc)
     user_messages = UserMessage.where(conversation_id: conversation.id).order(created_at: :asc)
@@ -17,7 +19,7 @@ class UserMessage < ApplicationRecord
       associated_user_message = user_messages.find { |user_msg| user_msg.bot_message_id == bot_message.id }
       messages << { role: "user", content: associated_user_message.content } if associated_user_message
     end
-    
+
     # Create a new bot message and pass the messages array to generate content
     bot = BotMessage.new(conversation: conversation)
     bot.save
