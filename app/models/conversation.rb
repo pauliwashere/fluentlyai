53f0c0cacaeeea20ce2361ec14cb21056ce6651b
Simon Pentzien <simon.pentzien@gmail.com>
2024-03-07 16:37:51 +0100

    sucks soooo much

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :topic
   belongs_to :user
-  has_many :bot_messages
-  has_many :user_messages
+  has_many :bot_messages, dependent: :destroy
+  has_many :user_messages, dependent: :destroy
 end
