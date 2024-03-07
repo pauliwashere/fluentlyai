4aae8ac696bd0e238bb2dfa922557aa43f68f9bb
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-03-07 15:04:40 +0100

    add open column to conversations

--- db/migrate/20240307133742_add_open_to_conversations.rb
+++ db/migrate/20240307133742_add_open_to_conversations.rb
@@ -1,5 +1,5 @@
 class AddOpenToConversations < ActiveRecord::Migration[7.1]
   def change
-    add_column :conversations, :open, :boolean
+    add_column :conversations, :open, :boolean, default: true
   end
 end
