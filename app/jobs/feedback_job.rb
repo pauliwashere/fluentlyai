f9a9e2b47d2562baf9fcbf8b0b2e32f4499a190e
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-03-11 15:19:43 +0100

    improving the prompt

--- app/jobs/feedback_job.rb
+++ app/jobs/feedback_job.rb
@@ -1,3 +1,19 @@
+# class FeedbackJob < ApplicationJob
+#   queue_as :default
+
+#   def perform(user_message)
+#     client = OpenAI::Client.new
+#     # messages = [{ role: "user", content: user_message.content }]
+#     chaptgpt_response = client.chat(parameters: {
+#       model: "gpt-3.5-turbo",
+#       # messages: messages
+#       messages: [{ role: "system", content: "Provide feedback on the #{user_message.content} in response to your previous #{bot_message.content}. Focus on grammar and vocabulary choice. After identifying grammatical mistakes or awkward phrasing, please suggest improvements while maintaining the context of the conversation. Each correction or improvement should be listed on a new line. If the  #{user_message.content} is already correct, do not make any changes and simply respond with 'Gut gemacht'."}]
+#       })
+#     feedback_content = chaptgpt_response["choices"][0]["message"]["content"]
+#     user_message.update(feedback: feedback_content)
+#     return feedback_content
+#   end
+# end
 class FeedbackJob < ApplicationJob
   queue_as :default
 
