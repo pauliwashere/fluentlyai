d593124e51720b5fc778c3b4ab0f44975caf0090
Paulina Kisielewska <paulina.kisielewska6@gmail.com>
2024-03-12 09:37:42 +0100

    Change show page and controller

--- app/javascript/controllers/conversation_window_scrolling_controller.js
+++ app/javascript/controllers/conversation_window_scrolling_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="conversation-window-scrolling"
 export default class extends Controller {
-
+  static targets = ["form"]
   connect() {
     console.log("hello")
     // const chat = document.getElementById("chat");
