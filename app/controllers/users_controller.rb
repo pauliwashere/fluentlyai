190e48db5c5db6473d1c7187d6ca8074eb36afab
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-03-14 15:15:05 +0100

    style levels page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:choose_language_level]
-  def choose_language_level
-
-  end
 
   def set_language_level
     current_user.update(level: params[:level])
